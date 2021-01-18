b29b4e9198acc9b117ad515227f30d80be72813d
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-17 22:26:00 -0500

    Started search entities method

--- js/index.js
+++ js/index.js
@@ -3,4 +3,25 @@ function redirect_to_about()
     window.location.href = "about.html";
 }
 
+function searchEntities()
+{
+    var search_txt = document.getElementById("searchBar").value;
+    if (search_txt != "")
+    { 
+        //clear out the result content
+        document.getElementById("").innerHTML = "";
 
+        var url = "https://aletheia.azurewebsites.net/api/SearchEntities?term=" + search_txt;
+        var req = new XMLHttpRequest();
+        req.open("get", url);
+        req.onreadystatechange = function()
+        {
+            if (req.readyState == 4 && req.status == 200)
+            {
+                console.log(req.responseText);
+            }
+        }
+        console.log("Sending...");
+        req.send();
+    }
+}
