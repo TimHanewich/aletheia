cf37a2b668f7ac618a02d1eff59119a9415f3174
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-17 22:41:48 -0500

    Search functionality

--- js/index.js
+++ js/index.js
@@ -18,10 +18,42 @@ function searchEntities()
         {
             if (req.readyState == 4 && req.status == 200)
             {
-                console.log(req.responseText);
+                
+                var results = JSON.parse(req.responseText);
+                results.forEach(addResultToSearchResults);
+
+                //Unhide the results
+                document.getElementById("search-results-master-cont").classList.remove("hidden");
             }
         }
         console.log("Sending...");
         req.send();
     }
+}
+
+function addResultToSearchResults(item, index)
+{
+    //Create the entry element
+    var ele = document.createElement("div");
+    ele.classList.add("search-result-entry");
+
+    //Same between both - CIK
+    var p_CIK = document.createElement("p");
+    p_CIK.classList.add("search-result-txt");
+    p_CIK.classList.add("search-result-col1");
+    p_CIK.innerText = item.CIK;
+    ele.appendChild(p_CIK);
+
+    var ENT_TYPE = item.entityType;
+    if (ENT_TYPE == "Person")
+    {
+        
+    }
+    else if (ENT_TYPE == "Company")
+    {
+
+    }
+
+    //Add it to the list
+    document.getElementById("search-results-list-cont").appendChild(ele);
 }
