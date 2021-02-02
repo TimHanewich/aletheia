a34c3a64dd273146c2c3689e63917f4b4e93ccb5
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-02-02 17:08:24 -0500

    Added scolling to endpoints

---
+++ js/api.js
@@ -0,0 +1,29 @@
+$("#navep_SearchPeople").click(function()
+{
+    $("#ep_SearchPeople")[0].scrollIntoView();
+});
+
+$("#navep_SearchCompanies").click(function()
+{
+    $("#ep_SearchCompanies")[0].scrollIntoView();
+});
+
+$("#navep_SearchEntities").click(function()
+{
+    $("#ep_SearchEntities")[0].scrollIntoView();
+});
+
+$("#navep_GetCompany").click(function()
+{
+    $("#ep_GetCompany")[0].scrollIntoView();
+});
+
+$("#navep_LatestTransactions").click(function()
+{
+    $("#ep_LatestTransactions")[0].scrollIntoView();
+});
+
+$("#navep_GetCompanyOwners").click(function()
+{
+    $("#ep_GetCompanyOwners")[0].scrollIntoView();
+});
