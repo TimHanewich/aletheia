0510fa8501544106342b9f72777063a34f1b6f0e
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-02-13 21:10:44 -0500

    Filled in username and account created @ date

--- myaccount/loadaccount.js
+++ myaccount/loadaccount.js
@@ -1,3 +1,21 @@
+//GLOBAL VARIABLES
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
 //Load the account
 var account_obj = GetAccountFromCookie();
-console.log(account_obj.Username);
+
+//If the account is null (the cookie was not found) go to the login page
+if (account_obj == null)
+{
+    window.location.href = "../login/";
+}
+
+document.getElementById("username").innerText = account_obj.Username;
+
+//Fill in the created @ date
+var d = new Date(account_obj.CreatedAtUtc);
+var d_as_str = monthNames[d.getMonth()]; //Month
+d_as_str = d_as_str + " " + (d.getDay() + 1).toString() + ", " + d.getFullYear();
+document.getElementById("account-created-at").innerText = d_as_str;
