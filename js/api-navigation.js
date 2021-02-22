7fe4de4b39ad342de5b2b31f99bd491af27c0e03
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-02-22 14:59:29 -0500

    Finished Search Entities endpoint

--- js/api-navigation.js
+++ js/api-navigation.js
@@ -5,34 +5,14 @@
 //ep_NAME
 //Then with JQuery (see below), create the click function which scrolls the documentation into view.
 
-$("#navep_SearchPeople").click(function()
-{
-    $("#ep_SearchPeople")[0].scrollIntoView();
-});
-
-$("#navep_SearchCompanies").click(function()
-{
-    $("#ep_SearchCompanies")[0].scrollIntoView();
-});
-
 $("#navep_SearchEntities").click(function()
 {
     $("#ep_SearchEntities")[0].scrollIntoView();
 });
 
-$("#navep_GetCompany").click(function()
-{
-    $("#ep_GetCompany")[0].scrollIntoView();
-});
-
 $("#navep_LatestTransactions").click(function()
 {
     $("#ep_LatestTransactions")[0].scrollIntoView();
-});
-
-$("#navep_GetCompanyOwners").click(function()
-{
-    $("#ep_GetCompanyOwners")[0].scrollIntoView();
 });
 
 $("#navep_LatestFilings").click(function()
