a4fc5918013d933fd12f74b0c1a47825927debef
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-03-04 12:15:05 -0500

    Added Terms of service to home page

--- js/homepage-nav.js
+++ js/homepage-nav.js
@@ -3,6 +3,11 @@ $("#tab_API").click(function()
     window.location.href = "api";
 });
 
+$("#tab_TermsOfService").click(function()
+{
+    window.location.href = "terms";
+})
+
 $("#tab_About").click(function()
 {
     window.location.href = "about.html";
