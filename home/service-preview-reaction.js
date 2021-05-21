8cd0b191d90ec15d68498b6e87059fe441d6be2c
Tim Hanewich <tahanewich@live.com>
2021-05-21 14:06:20 -0400

    Loaded insider trading on open

--- home/service-preview-reaction.js
+++ home/service-preview-reaction.js
@@ -118,4 +118,4 @@ async function SwitchServiceDescription(service_desc)
 $(".get-call-receive").fadeOut(1);
 
 //Start off by loading a service preview
-setTimeout(function(){document.getElementById("sp_StockData").click();}, 500);
+setTimeout(function(){document.getElementById("sp_InsiderTrading").click();}, 500);
