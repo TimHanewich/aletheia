83a85bcbca3fd73b400d351ee5e7e654502403cb
Tim Hanewich <tahanewich@live.com>
2021-06-30 22:27:41 -0400

    Moved title and published on tags to articles

--- article/article-loader.ts
+++ article/article-loader.ts
@@ -1,15 +1,12 @@
 function load_contents(file_name:string)
 {
-    console.log("Opening.");
     var req:XMLHttpRequest = new XMLHttpRequest();
     req.open("get", "./articles/" + file_name);
     req.onreadystatechange = function()
     {
         var content:string = req.responseText;
-        console.log("Content: " + content);
         (document.getElementById("center-cont") as HTMLElement).innerHTML = content;
     }
-    console.log("About to send");
     req.send();
 }
 
@@ -25,5 +22,9 @@ else
     if (idstr == "introducing-insider-trading-webhook")
     {
         load_contents("introducing-insider-trading-webhook.html");
+    }
+    else
+    {
+        console.log("ID '" + idstr + "' not recognized.");
     }
 }
