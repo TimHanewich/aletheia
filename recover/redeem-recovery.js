48924be89a3f01c0b8229903a6a7c2c3f9deb82f
Tim Hanewich <tahanewich@live.com>
2022-01-04 17:47:46 -0500

    Flip flop

--- recover/redeem-recovery.js
+++ recover/redeem-recovery.js
@@ -9,8 +9,20 @@ if (id != null) //If there IS an ID, show the password reset information. If the
 
     //Hide the initiation steps - they already did these!
     document.getElementById("recovery-initiation").style.display = 'none'; 
+
+    //Show the redeem steps
+    document.getElementById("recovery-redemption").style.display = 'block';
+
+    //Start off with a hidden message
+    HideRedeemMsg();
 }
 else
 {
     console.log("No password reset ID detected.");
+
+    //Hide the initiation steps - they already did these!
+    document.getElementById("recovery-initiation").style.display = 'block'; 
+
+    //Show the redeem steps
+    document.getElementById("recovery-redemption").style.display = 'none';
 }
--- recover/utilities.js
+++ recover/utilities.js
@@ -23,3 +23,29 @@ function ShowBtn()
 {
     document.getElementById("reset-button").style.display = 'block';
 }
+
+function HideRedeemMsg()
+{
+    document.getElementById("redeem-msg").style.display = 'none';
+}
+
+function ShowRedeemMsg()
+{
+    document.getElementById("redeem-msg").style.display = 'block';
+}
+
+function UpdateRedeemMsg(txt)
+{
+    ShowRedeemMsg();
+    document.getElementById("redeem-msg").innerText = txt;
+}
+
+function HideRedeemBtn()
+{
+    document.getElementById("redeem-button").style.display = 'none';
+}
+
+function ShowRedeemBtn()
+{
+    document.getElementById("redeem-button").style.display = 'block';
+}
