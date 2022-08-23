43417d7a85c6d52529d26f5d834cbcbce5dabe5b
Tim Hanewich <tahanewich@live.com>
2022-08-22 20:23:24 -0400

    Subscribe script

---
+++ subscribe/sub.js
@@ -0,0 +1,11 @@
+account = GetAccountFromCookie()
+
+if (account != null)
+{
+    var greeting = document.getElementById("greeting")
+    greeting.innerHTML = greeting.innerHTML.replace("Hello!", "Hello <b>" + account.Username + "</b>!");
+}
+else
+{
+    window.location.replace("../login/")
+}
